eec52c38ff8ef9046f4bfcc3ab255099c0b0b7b0
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-07-02 21:09:16 +0530

    ss

--- src/app/bus/busdata/busdata.component.ts
+++ src/app/bus/busdata/busdata.component.ts
@@ -9,25 +9,21 @@ import { bus } from 'src/app/busserives.service';
 })
 export class BusdataComponent {
   busesdata:any
-<<<<<<< HEAD
   searchFrom=""
   searchTo=""
-=======
-  buses:any[]=[];
->>>>>>> 082d2929445e1525918eb81e6c5300617b713649
   constructor(public busdata:BusserivesService){}
 
   ngOnInit(){
-    // this.busdata.getbus().subscribe((res:any)=>{
-    //   console.log(res)
-    //     this.busesdata=res;
-    // })
-    this.buses=this.busdata.getBus();
-    // this.busdata.getbus().subscribe((res:bus[])=>{
-    //   this.buses=res
-    //   console.log(this.buses)
-    // },error=>{
-    //   console.log("error", error)
-    // })
+    // // this.busdata.getbus().subscribe((res:any)=>{
+    // //   console.log(res)
+    // //     this.busesdata=res;
+    // // })
+    // this.buses=this.busdata.getBus();
+    // // this.busdata.getbus().subscribe((res:bus[])=>{
+    // //   this.buses=res
+    // //   console.log(this.buses)
+    // // },error=>{
+    // //   console.log("error", error)
+    // // })
   }
 }
--- src/app/busserives.service.ts
+++ src/app/busserives.service.ts
@@ -1,15 +1,60 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { busdata } from 'src/data/Bus'
+import { traindata } from 'src/data/Train'
+import { flightdata } from 'src/data/Flight'
+import { cabdata } from 'src/data/Cab'
+import { hoteldata } from 'src/data/Hotel'
+import { Observable } from 'rxjs';
+
+export interface bus {
+  bus_ID:number;
+  bus_Number:number;
+  bus_Name:string;
+  from_Location:string;
+  to_Location:string;
+  availble_Seats:number;
+  total_Seats:number
+
+}
 @Injectable({
   providedIn: 'root'
 })
 export class BusserivesService {
 
+  private businfo=busdata;
+  private traininfo=traindata;
+  private flightinfo=flightdata;
+  private cabinfo=cabdata;
+  private hotelinfo=hoteldata;
+
   constructor(public http:HttpClient) { }
-  busapiurl="http://localhost:3000/busdata"
-  seatapiurl="http://localhost:3000/seats"
-  getbus(){
-    return this.http.get(`${this.busapiurl}`)
+  seatapiurl=""
+  // busapiurl="http://localhost:3000/busdata"
+  // getbus(){
+  //   return this.http.get(`${this.busapiurl}`)
+  // }
+
+  // busApiUrl="https://localhost:7091/api/Buses";
+
+  // getbus(): Observable<bus[]>{
+  //   return this.http.get<bus[]>(this.busApiUrl);
+  // }
+
+  getBus(){
+    return this.businfo;
+  }
+  getTrain(){
+    return this.traininfo;
+  }
+  getFlight(){
+    return this.flightinfo;
+  }
+  getCab(){
+    return this.cabinfo;
+  }
+  getHotel(){
+    return this.hotelinfo;
   }
   getseat(){
     return this.http.get(`${this.seatapiurl}`)
